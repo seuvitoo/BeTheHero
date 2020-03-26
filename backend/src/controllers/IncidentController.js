b036ef913f92d66facaf968256b3009b16b7d3ea
vysantos <vy.santos@live.com>
2020-03-26 19:55:14 -0300

    correção controler incidentes-bug01

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -35,23 +35,21 @@ module.exports = {
         return response.json({ id });
     },
 
-    async delete(req, res) {
-        const { id } = req.params;
-        const ong_id = req.headers.authorization;
+    async delete(request, response) {
+        const { id } = request.params;
+        const ong_id = request.headers.authorization;
 
-        const incident = await connection("incidents")
-            .where("id", id)
-            .select("ong_id")
+        const incidents = await connection('incidents')
+            .where('id', id)
+            .select('ong_id')
             .first();
 
-        if (incident.ong_id !== ong_id) {
-            return res.status(401).json({ error: "Operation not permitted" });
+        if (incidents.ong_id != ong_id) {
+            return response.status(401).json({ error: 'Operation not permited.' })
         }
+        await connection('incidents')
+            .where('id', id).delete();
 
-        await connection("incidents")
-            .where("id", id)
-            .delete();
-
-        return res.status(204).send();
-    }
+        return response.status(204).send;
+    },
 };
