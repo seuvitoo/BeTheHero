dc4807f2e04f0a6c0d9886c6bfe09e05893c2f91
Victor dos Santos <vy.santos@live.com>
2020-04-01 18:30:48 -0300

    add unit test

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,3 +1,5 @@
+import geraId from '../utils/generateUniqueId';
+
 const crypto = require('crypto')
 const connection = require('../database/connection');
 
@@ -12,7 +14,7 @@ module.exports = {
 
     async create(request, response) {
         const { name, email, whatsapp, city, uf } = request.body;
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = geraId();
 
         await connection('ongs').insert({
             id, name, email, whatsapp, city, uf
